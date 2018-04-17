9c9c4af1826c41ee35dbe1995c7916a199e61cc8
Jan <jvanhoye@hotmail.com>
2018-04-17 10:22:48 +0200

    Domain Graph Update

---
+++ Integratie.Domain/Entities/Graph/BarChartGraph.cs
@@ -0,0 +1,25 @@
+﻿using Integratie.Domain.Entities.Subjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integratie.Domain.Entities.Graph
+{
+    public class BarChartGraph : Graph
+    {
+        public List<Subject> Subjects { get; set; }
+        public BarChartType Type { get; set; }
+    }
+    public enum BarChartType
+    {
+        
+        Count,
+        Male,
+        Female,
+        Sentiment, //gemiddelde
+        Age //gemiddelde
+
+    }
+}
