167ce27f17a1611b2a3eb77ec842447e7d91735e
Marnick Huysmans <marnick.huysmans@student.kdg.be>
2018-04-17 15:49:11 +0200

    init 2

--- Integratie.Domain/Entities/Graph/LineGraph.cs
+++ Integratie.Domain/Entities/Graph/LineGraph.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Integratie.Domain.Entities.Subjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace Integratie.Domain.Entities.Graph
 {
     public class LineGraph : Graph
     {
+        public Subject Subject { get; set; }
+        public Period Period { get; set; }
+    }
+    public enum Period
+    {
+        DAY,
+        WEEK
     }
 }
