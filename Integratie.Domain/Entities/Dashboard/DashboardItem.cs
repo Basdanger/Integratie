183765b25409ded9f67ce326a9de23dd5613305e
Jan <jvanhoye@hotmail.com>
2018-04-23 22:14:37 +0200

    Dashboard Serialization init

---
+++ Integratie.Domain/Entities/Dashboard/DashboardItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Integratie.Domain.Entities.Graph;
+
+namespace Integratie.Domain.Entities.Dashboard
+{
+    public class DashboardItem
+    {
+        public int Id { get; set; }
+        public int Column { get; set; }
+        public int Row { get; set; }
+        public int X_Size { get; set; }
+        public int Y_Size { get; set; }
+        public Graph.Graph Graph { get; set; }
+
+        public DashboardItem()
+        {
+
+        }
+
+        public DashboardItem(int id, int column, int row, int x_Size, int y_Size, Graph.Graph graph)
+        {
+            Id = id;
+            Column = column;
+            Row = row;
+            X_Size = x_Size;
+            Y_Size = y_Size;
+            Graph = graph;
+        }
+    }
+}
