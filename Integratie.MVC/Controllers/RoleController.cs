e568c9fc20df60bb601baf7cf30fcaf4fbaf52d5
Unknown <nikolaas.deburggrave@student.kdg.be>
2018-04-24 09:36:57 +0200

    SuperAdmin + roles + user logon information

---
+++ Integratie.MVC/Controllers/RoleController.cs
@@ -0,0 +1,98 @@
+﻿using Integratie.MVC.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+
+namespace Integratie.MVC.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class RoleController : Controller
+    {
+        private ApplicationDbContext context;
+
+        public RoleController()
+        {
+            context = new ApplicationDbContext();
+        }
+        
+
+        // GET: Role
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (!isAdminUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var roles = context.Roles.ToList();
+            return View(roles);
+        }
+
+        public Boolean isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var role = userManager.GetRoles(user.GetUserId());
+                if (role[0].ToString() == "SuperAdmin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public ActionResult Create()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (!isAdminUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+
+        [HttpPost]
+        public ActionResult Create(IdentityRole Role)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (!isAdminUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            context.Roles.Add(Role);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
