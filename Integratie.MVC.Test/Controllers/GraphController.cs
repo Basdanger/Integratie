e0f8fcc78452f0048afae218ea78e3a971b77d14
Marnick Huysmans <marnick.huysmans@student.kdg.be>
2018-04-27 15:03:39 +0200

    Partial graph views

---
+++ Integratie.MVC.Test/Controllers/GraphController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Integratie.MVC.Test.Controllers
+{
+    public class GraphController : Controller
+    {
+        public ActionResult _LineGraphCompare(string id)
+        {
+            ViewBag.id = id;
+            return PartialView();
+        }
+        public ActionResult _BarGraphCompare(string id)
+        {
+            ViewBag.id = id;
+            return PartialView();
+        }
+        public ActionResult _DoughnutGraph(string id)
+        {
+            ViewBag.id = id;
+            return PartialView();
+        }
+    }
+}
