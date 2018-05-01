ad0d0e7df73e95d70e12d9d768634dc31996e43b
Yannis Arnouts <yannis.arnouts@student.kdg.be>
2018-05-01 17:30:03 +0200

    xd

---
+++ Integratie.MVC/Controllers/Api/SubjectManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Integratie.MVC.Controllers.Api
+{
+    internal class SubjectManager
+    {
+        internal object GetSubjects()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
