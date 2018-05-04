dbdb3259d082b8fabb4b1a65ace1a0a03e60f74f
Yannis Arnouts <yannis.arnouts@student.kdg.be>
2018-05-04 17:41:56 +0200

    a

---
+++ Integratie.MVC/Controllers/PersoonController.cs
@@ -0,0 +1,28 @@
+﻿using Integratie.BL.Managers;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace Integratie.MVC.Controllers
+{
+    public class PersoonController : Controller
+    {
+        private SubjectManager manager = new SubjectManager();
+        // GET: Persoon
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Personen()
+        {
+            var subjects = manager.GetSubjects();
+            return View(subjects);
+        }
+    }
+}
