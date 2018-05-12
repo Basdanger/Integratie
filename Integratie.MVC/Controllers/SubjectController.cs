61eb325f7bdc22b99f85d3556bce1412fbbfb528
yannisarnouts <yannis.arnouts@student.kdg.be>
2018-05-12 19:07:42 +0200

    csv uploaden

--- Integratie.MVC/Controllers/SubjectController.cs
+++ Integratie.MVC/Controllers/SubjectController.cs
@@ -1,10 +1,13 @@
 ﻿using CsvHelper;
+using ExcelDataReader;
 using Integratie.BL.Managers;
 using Integratie.Domain.Entities;
 using Integratie.Domain.Entities.Subjects;
 using Integratie.MVC.Models;
+using LINQtoCSV;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Dynamic;
 using System.IO;
@@ -260,57 +263,38 @@ namespace Integratie.MVC.Controllers
             return View(people);
         }
         //[HttpPost]
-        //public void UploadPerson(HttpPostedFileBase file)
+        //public void UploadCsv(HttpPostedFileBase attachmentcsv)
         //{
-        //    using (var reader = new StreamReader(file.InputStream))
+        //    CsvFileDescription csvFileDescription = new CsvFileDescription
         //    {
-        //        List<Person> personen = new List<Person>();
-        //        List<String> First_Name = new List<string>();
-        //        List<String> Last_Name = new List<string>();
-        //        while (!reader.EndOfStream)
-        //        {
-        //            var line = reader.ReadLine();
-        //            var values = line.Split(',');
-
-        //            First_Name.Add(values[0]);
-        //            Last_Name.Add(values[1]);
-        //        }
-        //        for (int i = 0; i < First_Name.Count; i++)
-        //        {
-        //            personen.Add(new Person(First_Name[i], Last_Name[i]));
-        //        }
-        //        mgr.CreatePersons(personen);
+        //        SeparatorChar = ',',
+        //        FirstLineHasColumnNames = true
+        //    };
+        //    CsvContext csvContext = new CsvContext();
+        //    StreamReader streamReader = new StreamReader(attachmentcsv.InputStream);
+        //    IEnumerable<Person> list = csvContext.Read<Person>(streamReader, csvFileDescription);
+        //    foreach (var item in list)
+        //    {
+        //        mgr.AddPerson(item.First_Name, item.Last_Name, item.District, item.Level, item.Gender, item.Twitter, item.Site, item.DateOfBirth, item.Facebook, item.Postal_Code, item.Full_Name, item.Position, item.Organisation, item.Town);
         //    }
+        //    //return Redirect("Personen");
         //}
         [HttpPost]
-        public ActionResult UploadPerson(HttpPostedFileBase file)
+        public ActionResult CSVOpladen(HttpPostedFileBase attachmentcsv)
         {
-            string path = null;
-            List<Person> personen = new List<Person>();
-            try
+            CsvFileDescription csvFileDescription = new CsvFileDescription
             {
-                if(file.ContentLength > 0)
-                {
-                    var fileName = Path.GetFileName(file.FileName);
-                    path = AppDomain.CurrentDomain.BaseDirectory + "upload\\" + fileName;
-                    file.SaveAs(path);
-
-                    var csv = new CsvReader(new StreamReader(path));
-                    var PersonList = csv.GetRecords<Person>();
-
-                    foreach (var item in PersonList)
-                    {
-                        Person person = new Person();
-
-                        person.First_Name = item.First_Name;
-                        person.Last_Name = item.Last_Name;
-
-                        personen.Add(person);
-                    }
-                }
+                SeparatorChar = ',',
+                FirstLineHasColumnNames = true
+            };
+            CsvContext csvContext = new CsvContext();
+            StreamReader streamReader = new StreamReader(attachmentcsv.InputStream);
+            IEnumerable<Person> list = csvContext.Read<Person>(streamReader, csvFileDescription);
+            foreach (var item in list)
+            {
+                mgr.AddPerson(item.First_Name, item.Last_Name, item.District, item.Level, item.Gender, item.Twitter, item.Site, item.DateOfBirth, item.Facebook, item.Postal_Code, item.Full_Name, item.Position, item.Organisation, item.Town);
             }
-            catch { }
-            return View(personen);
+            return Redirect("Personen");
         }
     }
 }
