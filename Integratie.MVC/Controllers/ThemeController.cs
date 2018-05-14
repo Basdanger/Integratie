91fd1ae79c2e6980d2bf057d0a0c86dd7f6e139f
Jan <jvanhoye@hotmail.com>
2018-05-14 14:49:47 +0200

    Init

---
+++ Integratie.MVC/Controllers/ThemeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Integratie.MVC.Controllers
+{
+    public class ThemeController : Controller
+    {
+        // GET: Theme
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
