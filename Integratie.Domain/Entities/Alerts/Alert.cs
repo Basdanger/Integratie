60fc9903100450126e44c1b0058d7958d725794d
Marnick Huysmans <marnick.huysmans@student.kdg.be>
2018-05-18 10:51:57 +0200

    alert graph start

--- Integratie.Domain/Entities/Alerts/Alert.cs
+++ Integratie.Domain/Entities/Alerts/Alert.cs
@@ -13,5 +13,6 @@ namespace Integratie.Domain.Entities.Alerts
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int AlertID { get; set; }
         public bool Ring { get; set; } = false;
+        public virtual Graph.Graph Graph { get; set; }
     }
 }
