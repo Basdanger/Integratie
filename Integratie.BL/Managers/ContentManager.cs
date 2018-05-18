5227f8f2788c56645a7b6203d4c97b5d3656e795
Jan <jvanhoye@hotmail.com>
2018-05-18 13:45:19 +0200

    c

---
+++ Integratie.BL/Managers/ContentManager.cs
@@ -0,0 +1,27 @@
+﻿using Integratie.DAL.Repositories;
+using Integratie.Domain.Entities.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integratie.BL.Managers
+{
+    public class ContentManager
+    {
+        ContentRepo repo = new ContentRepo();
+        public SiteContent AddSiteContent(SiteContent sc)
+        {
+            return repo.AddSiteContent(sc);
+        }
+        public bool RemoveSiteContent(SiteContent sc)
+        {
+            return repo.DeleteSiteContent(sc);
+        }
+        public bool UpdateSiteContent(SiteContent sc)
+        {
+            return repo.UpdateSiteContent(sc);
+        }
+    }
+}
---
+++ Integratie.MVC/Controllers/SiteContentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Integratie.MVC.Controllers
+{
+    public class SiteContentController : Controller
+    {
+        // GET: SiteContent
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
