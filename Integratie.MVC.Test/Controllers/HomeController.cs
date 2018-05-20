5a37760db6c4203401e82f2d1a0824081b49af3f
Jan <jvanhoye@hotmail.com>
2018-05-20 13:37:14 +0200

    mini fix

--- Integratie.MVC.Test/Controllers/HomeController.cs
+++ Integratie.MVC.Test/Controllers/HomeController.cs
@@ -29,13 +29,13 @@ namespace Integratie.MVC.Test.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-            List<double> listValues = new List<double>();
-            List<string> listKeys = new List<string>();
-            BarChartGraph graph = (BarChartGraph)manager.GetAllFilledGraphs().First();
-            graph.Values.Values.ToList().ForEach(i => listValues.Add(i));
-            graph.Values.Keys.ToList().ForEach(i => listKeys.Add(i));
-            ViewBag.DataValues = JsonConvert.SerializeObject(listValues);
-            ViewBag.DataKeys = JsonConvert.SerializeObject(listKeys);
+            //List<double> listValues = new List<double>();
+            //List<string> listKeys = new List<string>();
+            //BarChartGraph graph = (BarChartGraph)manager.GetAllFilledGraphs().First();
+            //graph.Values.Values.ToList().ForEach(i => listValues.Add(i));
+            //graph.Values.Keys.ToList().ForEach(i => listKeys.Add(i));
+            //ViewBag.DataValues = JsonConvert.SerializeObject(listValues);
+            //ViewBag.DataKeys = JsonConvert.SerializeObject(listKeys);
             return View();
         }
     }
