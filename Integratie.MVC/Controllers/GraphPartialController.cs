4bc087a03c554fb391cae20be32562129b07d1a1
MarnickH <marnick.huysmans@student.kdg.be>
2018-05-24 20:46:54 +0200

    personen update

--- Integratie.MVC/Controllers/GraphPartialController.cs
+++ Integratie.MVC/Controllers/GraphPartialController.cs
@@ -1,4 +1,5 @@
 ﻿using Integratie.BL.Managers;
+using Integratie.Domain.Entities.Alerts;
 using Integratie.Domain.Entities.Graph;
 using Newtonsoft.Json;
 using System;
@@ -13,18 +14,19 @@ namespace Integratie.MVC.Controllers
     {
         GraphManager graphManager = new GraphManager();
         // GET: GraphPartial
-        public ActionResult _BarChart(int id)
+        public ActionResult _BarChart(Alert alert)
         {
-            //ViewBag.id = id;
-            //BarChartGraph graph = graphManager.GetBarChartGraph(id);
-            //List<double> listValues = graph.Values.Values.ToList();
-            //List<string> listKeys = graph.Values.Keys.ToList();
-            //ViewBag.Values = JsonConvert.SerializeObject(listValues);
-            //ViewBag.Keys = JsonConvert.SerializeObject(listKeys);
-            return PartialView();
+            alert.Graph.BarValues = JsonConvert.DeserializeObject<Dictionary<string,double>>(alert.JsonValues);
+            return PartialView(alert);
         }
 
-        public ActionResult _PieChart(int id)
+        public ActionResult _LineChart(Alert alert)
+        {
+            alert.Graph.LineValues = JsonConvert.DeserializeObject<Dictionary<string, List<double>>>(alert.JsonValues);
+            return PartialView(alert);
+        }
+
+        public ActionResult _PieChart(Alert alert)
         {
             //ViewBag.id = id;
             //PieGraph graph = graphManager.GetPieGraph(id);
